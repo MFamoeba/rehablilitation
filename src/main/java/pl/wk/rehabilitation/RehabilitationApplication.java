a909ba5120d5c1fb92142ae93dddd319e739c4e5
komputer stacjonarny <wkulesza298@gmail.com>
2025-12-01 16:39:28 +0100

    Init

---
+++ src/main/java/pl/wk/rehabilitation/RehabilitationApplication.java
@@ -0,0 +1,13 @@
+package pl.wk.rehabilitation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RehabilitationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RehabilitationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/wk/rehabilitation/RehabilitationApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.wk.rehabilitation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RehabilitationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
